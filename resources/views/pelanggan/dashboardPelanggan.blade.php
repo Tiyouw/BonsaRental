63ddfec23ade9a470ef972fffb27ebd195fb0c76
Ahimsa Jenar Bramsaifstyo Kusuma <tio.rmx@gmail.com>
2025-06-15 21:17:28 +0700

    Tiyo

--- resources/views/pelanggan/dashboardPelanggan.blade.php
+++ resources/views/pelanggan/dashboardPelanggan.blade.php
@@ -33,7 +33,7 @@
                 @endif
 
                 <!-- Link ke detail produk -->
-                <a href="{{ route('detailProduk', $item['id']) }}" class="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary/80 transition mt-4 inline-block">Sewa Sekarang</a>
+                <a href="{{ route('detailProduk', $item['id']) }}" class="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary/80 transition mt-4 inline-block">Sewa Sekarang!</a>
             </div>
         @endforeach
     </div>
